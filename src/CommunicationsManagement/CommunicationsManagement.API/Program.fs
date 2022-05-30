0d9bba3b79cf7cd8bdac5a5acca4b5d88c58926e
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-05-30 11:38:17 +0200

    walking skeleton

---
+++ src/CommunicationsManagement/CommunicationsManagement.API/Program.fs
@@ -0,0 +1,109 @@
+﻿open System
+open System.Collections.Generic
+open System.IO
+open System.Text
+open System.Threading
+open System.Threading.Tasks
+open EventStore.Client
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+open Newtonsoft.Json
+
+[<CLIMutable>]
+type Configuration = { EventStoreConnectionString: string }
+
+[<CLIMutable>]
+type Message = { ID: int; Amount: int }
+
+let configuration =
+  ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", true, true)
+    .AddEnvironmentVariables()
+    .Build()
+    .Get<Configuration>()
+
+let settings =
+  EventStoreClientSettings.Create configuration.EventStoreConnectionString
+
+let client = new EventStoreClient(settings)
+
+let state = Dictionary<int, int>()
+
+let mutable checkpoint: StreamPosition option = None
+
+let getCheckpoint () =
+  checkpoint
+  |> Option.map FromStream.After
+  |> Option.defaultValue FromStream.Start
+
+let handleEvent (evnt: ResolvedEvent) =
+  task {
+    let data = evnt.Event.Data.ToArray()
+    let decoded = Encoding.UTF8.GetString(data)
+    let parsed = JsonConvert.DeserializeObject<Message>(decoded)
+
+    if not <| state.ContainsKey(parsed.ID) then
+      state[parsed.ID] <- parsed.Amount
+    else
+      state[parsed.ID] <- state[parsed.ID] + parsed.Amount
+
+    checkpoint <- Some evnt.OriginalEventNumber
+
+    return ()
+  }
+
+let rec subscribe () =
+  let reSubscribe (_: StreamSubscription) (reason: SubscriptionDroppedReason) (_: Exception) =
+    if reason = SubscriptionDroppedReason.Disposed |> not then
+      subscribe () |> ignore
+    else
+      ()
+
+  let handle _ evnt _ = task { do! handleEvent evnt } :> Task
+
+  task {
+    try
+      let! _ =
+        client.SubscribeToStreamAsync("deletable", getCheckpoint (), handle, false, reSubscribe)
+
+      return ()
+    with
+    | _ ->
+      do! Task.Delay(5000)
+      return! subscribe ()
+  }
+
+subscribe () |> ignore
+
+let getState = warbler (fun _ -> json state)
+
+let webApp =
+  choose [ route "/ping" >=> text "pong"
+           route "/inventory" >=> getState
+           route "/" >=> htmlFile "./pages/index.html" ]
+
+let configureApp (app: IApplicationBuilder) = app.UseGiraffe webApp
+
+let configureServices (services: IServiceCollection) =
+  // Add Giraffe dependencies
+  services.AddGiraffe() |> ignore
+
+[<EntryPoint>]
+let main _ =
+  Host
+    .CreateDefaultBuilder()
+    .ConfigureWebHostDefaults(fun webHostBuilder ->
+      webHostBuilder
+        .Configure(configureApp)
+        .ConfigureServices(configureServices)
+      |> ignore)
+    .Build()
+    .Run()
+
+  0
