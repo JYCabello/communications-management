7ec3f4800e7317dc1bc72b068fe25c7ffbd41df5
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-05-31 12:07:13 +0200

    Extracted eventstore, not refactored

---
+++ src/CommunicationsManagement/CommunicationsManagement.API/EventStore.fs
@@ -0,0 +1,63 @@
+﻿module CommunicationsManagement.API.EventStore
+
+open System
+open System.Collections.Generic
+open System.Text
+open System.Threading.Tasks
+open Models
+open EventStore.Client
+open Configuration
+open Newtonsoft.Json
+
+let settings =
+  EventStoreClientSettings.Create configuration.EventStoreConnectionString
+
+let client = new EventStoreClient(settings)
+
+let state = Dictionary<int, int>()
+
+let mutable checkpoint: StreamPosition option = None
+
+let getCheckpoint () =
+  checkpoint
+  |> Option.map FromStream.After
+  |> Option.defaultValue FromStream.Start
+
+let handleEvent (evnt: ResolvedEvent) =
+  task {
+    let data = evnt.Event.Data.ToArray()
+    let decoded = Encoding.UTF8.GetString(data)
+    let parsed = JsonConvert.DeserializeObject<Message>(decoded)
+
+    if not <| state.ContainsKey(parsed.ID) then
+      state[parsed.ID] <- parsed.Amount
+    else
+      state[parsed.ID] <- state[parsed.ID] + parsed.Amount
+
+    checkpoint <- Some evnt.OriginalEventNumber
+
+    return ()
+  }
+
+let rec subscribe () =
+  let reSubscribe (_: StreamSubscription) (reason: SubscriptionDroppedReason) (_: Exception) =
+    if reason = SubscriptionDroppedReason.Disposed |> not then
+      subscribe () |> ignore
+    else
+      ()
+
+  let handle _ evnt _ = task { do! handleEvent evnt } :> Task
+
+  task {
+    try
+      return!
+        client
+          .SubscribeToStreamAsync("deletable", getCheckpoint(), handle, false, reSubscribe)
+          :> Task
+    with
+    | _ ->
+      do! Task.Delay(5000)
+      return! subscribe ()
+  }
+
+subscribe () |> ignore
--- src/CommunicationsManagement/CommunicationsManagement.API/Program.fs
+++ src/CommunicationsManagement/CommunicationsManagement.API/Program.fs
@@ -9,62 +9,8 @@ open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
-open Newtonsoft.Json
-open Models
-open Configuration
+open EventStore
 
-let settings =
-  EventStoreClientSettings.Create configuration.EventStoreConnectionString
-
-let client = new EventStoreClient(settings)
-
-let state = Dictionary<int, int>()
-
-let mutable checkpoint: StreamPosition option = None
-
-let getCheckpoint () =
-  checkpoint
-  |> Option.map FromStream.After
-  |> Option.defaultValue FromStream.Start
-
-let handleEvent (evnt: ResolvedEvent) =
-  task {
-    let data = evnt.Event.Data.ToArray()
-    let decoded = Encoding.UTF8.GetString(data)
-    let parsed = JsonConvert.DeserializeObject<Message>(decoded)
-
-    if not <| state.ContainsKey(parsed.ID) then
-      state[parsed.ID] <- parsed.Amount
-    else
-      state[parsed.ID] <- state[parsed.ID] + parsed.Amount
-
-    checkpoint <- Some evnt.OriginalEventNumber
-
-    return ()
-  }
-
-let rec subscribe () =
-  let reSubscribe (_: StreamSubscription) (reason: SubscriptionDroppedReason) (_: Exception) =
-    if reason = SubscriptionDroppedReason.Disposed |> not then
-      subscribe () |> ignore
-    else
-      ()
-
-  let handle _ evnt _ = task { do! handleEvent evnt } :> Task
-
-  task {
-    try
-      return!
-        client
-          .SubscribeToStreamAsync("deletable", getCheckpoint(), handle, false, reSubscribe)
-          :> Task
-    with
-    | _ ->
-      do! Task.Delay(5000)
-      return! subscribe ()
-  }
-
-subscribe () |> ignore
 
 let (>>=>) a b =
   a >=> warbler (fun _ -> b)
