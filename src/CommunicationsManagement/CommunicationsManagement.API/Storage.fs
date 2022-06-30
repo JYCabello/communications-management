9a0e6941a6dc37006c24dfe41e07e834753e52ca
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-06-30 16:24:44 +0200

    channels storage

--- src/CommunicationsManagement/CommunicationsManagement.API/Storage.fs
+++ src/CommunicationsManagement/CommunicationsManagement.API/Storage.fs
@@ -39,6 +39,7 @@ module Sessions =
     sessionStorage.TryRemove(id) |> ignore
     Task.singleton ()
 
+[<RequireQualifiedAccess>]
 module Users =
   let private userStorage = ConcurrentDictionary<Guid, User>()
 
@@ -68,10 +69,32 @@ module Users =
     |> ignore
     |> Task.singleton
 
+[<RequireQualifiedAccess>]
+module Channels =
+  let private channelStorage = ConcurrentDictionary<Guid, Channel>()
 
-let private channelStorage = ConcurrentDictionary<Guid, Channel>()
+  let query id =
+    channelStorage |> tryGet id |> Task.singleton
 
+  let getAll () =
+    channelStorage.Values
+    |> Seq.sortByDescending (fun c -> c.IsEnabled)
+    |> Seq.toList
+    |> TaskResult.ok
 
+  let find q =
+    channelStorage.ToArray()
+    |> Seq.tryFind (fun kvp -> q kvp.Value)
+    |> Option.map (fun kvp -> kvp.Value)
+    |> Task.singleton
+
+  let save (c: Channel) =
+    Task.singleton <| channelStorage[c.ID] <- c
+
+  let delete (id: Guid) =
+    channelStorage.TryRemove(id)
+    |> ignore
+    |> Task.singleton
 
 let optionToObjResult<'a> (topt: Task<'a option>) =
   topt
@@ -86,6 +109,7 @@ let query<'a> : Configuration -> Guid -> Task<Result<'a, DomainError>> =
         match typeof<'a> with
         | t when t = typeof<Session> -> Sessions.query id |> optionToObjResult<Session>
         | t when t = typeof<User> -> Users.query id |> optionToObjResult<User>
+        | t when t = typeof<Channel> -> Channels.query id |> optionToObjResult<Channel>
         | t ->
           InternalServerError $"Query not implemented for type {t.FullName}"
           |> TaskResult.error
@@ -100,6 +124,7 @@ let getAll<'a> : Configuration -> unit -> Task<Result<'a list, DomainError>> =
         match typeof<'a> with
         | t when t = typeof<Session> -> Sessions.getAll () |> TaskResult.map box
         | t when t = typeof<User> -> Users.getAll () |> TaskResult.map box
+        | t when t = typeof<Channel> -> Channels.getAll () |> TaskResult.map box
         | t ->
           InternalServerError $"Query not implemented for type {t.FullName}"
           |> TaskResult.error
@@ -120,6 +145,10 @@ let queryPredicate<'a> : Configuration -> ('a -> bool) -> Task<Result<'a, Domain
           box predicate :?> User -> bool
           |> Users.find
           |> optionToObjResult<User>
+        | t when t = typeof<Channel> ->
+          box predicate :?> Channel -> bool
+          |> Channels.find
+          |> optionToObjResult<Channel>
         | t ->
           InternalServerError $"Query not implemented for type {t.FullName}"
           |> TaskResult.error
@@ -134,6 +163,7 @@ let save<'a> : Configuration -> 'a -> Task<Result<unit, DomainError>> =
         match typeof<'a> with
         | t when t = typeof<Session> -> box a :?> Session |> Sessions.save |> Task.map Ok
         | t when t = typeof<User> -> box a :?> User |> Users.save |> Task.map Ok
+        | t when t = typeof<Channel> -> box a :?> Channel |> Channels.save |> Task.map Ok
         | t ->
           InternalServerError $"Save not implemented for type {t.FullName}"
           |> TaskResult.error
@@ -146,6 +176,7 @@ let delete<'a> : Configuration -> Guid -> Task<Result<unit, DomainError>> =
         match typeof<'a> with
         | t when t = typeof<Session> -> Sessions.delete id |> Task.map Ok
         | t when t = typeof<User> -> Users.delete id |> Task.map Ok
+        | t when t = typeof<Channel> -> Channels.delete id |> Task.map Ok
         | t ->
           InternalServerError $"Delete not implemented for type {t.FullName}"
           |> TaskResult.error
