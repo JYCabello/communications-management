053a8666e4f249d775dfce8ca64db5b4d6e591cd
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-06-30 17:00:45 +0200

    channel list view complete

--- src/CommunicationsManagement/CommunicationsManagement.API/Views/Channels/ListChannels.fs
+++ src/CommunicationsManagement/CommunicationsManagement.API/Views/Channels/ListChannels.fs
@@ -9,12 +9,52 @@ open Giraffe.ViewEngine
 type ChannelListViewModel = { Channels: Channel list }
 
 let list (vm: ViewModel<ChannelListViewModel>) : XmlNode list =
+  let trx = vm.Root.Translate
+  let baseUrl = vm.Root.BaseUrl
+
   let newChannelUrl =
-    vm
-      .Root
-      .BaseUrl
+    baseUrl
       .AppendPathSegments("channels", "create")
       .ToString()
+
+  let enableLink (c: Channel) =
+    let enableUrl =
+      baseUrl
+        .AppendPathSegments("channels", c.ID, "enable")
+        .ToString()
+
+    a [ _href enableUrl
+        _class "btn btn-success btn-sm enable-channel-link" ] [
+      "Enable" |> trx |> Text
+    ]
+
+  let disableLink (c: Channel) =
+    let enableUrl =
+      baseUrl
+        .AppendPathSegments("channels", c.ID, "disable")
+        .ToString()
+
+    a [ _href enableUrl
+        _class "btn btn-danger btn-sm disable-channel-link" ] [
+      "Disable" |> trx |> Text
+    ]
+
+  let row (c: Channel) =
+    div [ _class "row" ] [
+      div [ _class "col" ] [ c.Name |> Text ]
+      div [ _class "col" ] [
+        match c.IsEnabled with
+        | true -> "Enabled"
+        | false -> "Disabled"
+        |> trx
+        |> Text
+      ]
+      div [ _class "col" ] [
+        match c.IsEnabled with
+        | true -> disableLink c
+        | false -> enableLink c
+      ]
+    ]
 
   [ div [ _class "d-flex flex-row-reverse" ] [
       a [ _href newChannelUrl
@@ -22,4 +62,18 @@ let list (vm: ViewModel<ChannelListViewModel>) : XmlNode list =
           _id "new-channel-link" ] [
         "New" |> vm.Root.Translate |> Text
       ]
+    ]
+    div [ _class "container" ] [
+      div [ _class "row p-2 bd-highlight" ] [
+        div [ _class "col" ] [
+          "Name" |> vm.Root.Translate |> Text
+        ]
+        div [ _class "col" ] [
+          "Status" |> vm.Root.Translate |> Text
+        ]
+        div [ _class "col" ] [
+          "Actions" |> vm.Root.Translate |> Text
+        ]
+      ]
+      yield! vm.Model.Channels |> List.map row
     ] ]
