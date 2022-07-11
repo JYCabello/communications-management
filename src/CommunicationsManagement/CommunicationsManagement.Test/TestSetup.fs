2b598ab87ca8ca7c40d44d1bf76949742728fa4f
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-07-11 11:30:59 +0200

    starting azurite in test

--- src/CommunicationsManagement/CommunicationsManagement.Test/TestSetup.fs
+++ src/CommunicationsManagement/CommunicationsManagement.Test/TestSetup.fs
@@ -87,6 +87,27 @@ let private startContainer (cp: CreateContainerParameters) =
 
     return id
   }
+  
+let private azuriteCreateParams port =
+  let name = $"comm-mgmt-test-azurite-deleteme%i{port}"
+  let hostConfig =
+    let hostConfig = HostConfig()
+
+    let storage =
+      PortBinding()
+      |> fun b ->
+           b.HostPort <- $"%i{port}/tcp"
+           b.HostIP <- "0.0.0.0"
+           b
+
+    hostConfig.PortBindings <- Dictionary<string, IList<PortBinding>>()
+    hostConfig.PortBindings.Add("10000/tcp", List<PortBinding>([ storage ]))
+    hostConfig
+  let createParams = CreateContainerParameters()
+  createParams.Name <- name
+  createParams.Image <- "mcr.microsoft.com/azure-storage/azurite"
+  createParams.HostConfig <- hostConfig
+  createParams
 
 let private eventStoreCreateParams port =
   let name = $"comm-mgmt-test-event-store-db-deleteme%i{port}"
@@ -148,11 +169,17 @@ let getFreePort () =
 
 let testSetup () =
   task {
-    let containerPort = getFreePort ()
+    let eventStorePort = getFreePort ()
 
     let! eventStoreContainerID =
       startContainer
-      <| eventStoreCreateParams containerPort
+      <| eventStoreCreateParams eventStorePort
+
+    let azuritePort = getFreePort ()
+
+    let! azuriteContainerID =
+      startContainer
+      <| azuriteCreateParams azuritePort
 
     let driver =
       let driverOptions =
@@ -173,7 +200,7 @@ let testSetup () =
     let baseUrl = $"http://localhost:{sitePort}"
 
     let config =
-      { EventStoreConnectionString = $"esdb://admin:changeit@localhost:{containerPort}?tls=false"
+      { EventStoreConnectionString = $"esdb://admin:changeit@localhost:{eventStorePort}?tls=false"
         BaseUrl = baseUrl
         AdminEmail = "notareal@email.com"
         SendGridKey = ""
@@ -204,7 +231,8 @@ let testSetup () =
       }
 
     let disposers =
-      [ fun () -> deleteContainer eventStoreContainerID |> fun t -> t.Result
+      [ fun () -> eventStoreContainerID |> deleteContainer |> fun t -> t.Result
+        fun () -> azuriteContainerID |> deleteContainer  |> fun t -> t.Result
         host.Dispose
         driver.Dispose ]
 
