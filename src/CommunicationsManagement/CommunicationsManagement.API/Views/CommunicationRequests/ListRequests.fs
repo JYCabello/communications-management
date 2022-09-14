04bba87c436520f04abead4302b872d6e95f0448
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-09-14 12:42:31 +0200

    we have the link

--- src/CommunicationsManagement/CommunicationsManagement.API/Views/CommunicationRequests/ListRequests.fs
+++ src/CommunicationsManagement/CommunicationsManagement.API/Views/CommunicationRequests/ListRequests.fs
@@ -1,9 +1,24 @@
 ﻿module CommunicationsManagement.API.Views.CommunicationRequests.ListRequests
 
-open CommunicationsManagement.API.Models
+open CommunicationsManagement.API
+open Models
 open Giraffe.ViewEngine.HtmlElements
+open Urls
+open Giraffe.ViewEngine
 
 type ListRequestViewModel =
   { InEdition: EditingCommunicationsRequest list }
 
-let list (vm: ViewModel<ListRequestViewModel>) : XmlNode list = []
+let list (vm: ViewModel<ListRequestViewModel>) : XmlNode list =
+  let trxTxt = vm.Root.Translate >> Text
+  let baseUrl = vm.Root.BaseUrl
+
+  let newCommReqUrl = urlFor baseUrl [ "communication-requests"; "create" ] []
+
+  [ div [ _class "d-flex flex-row-reverse" ] [
+      a [ _href newCommReqUrl
+          _class "btn btn-success btn-sm user-link"
+          _id "new-request-link" ] [
+        "New" |> trxTxt
+      ]
+    ] ]
